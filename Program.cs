e0424ed82b69e71631de982fe3523a412ff7b92e
Lucas Mota <lucasmota7@gmail.com>
2021-11-27 13:12:51 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Informe a metragem da frente do imovel: ");
+            double frente = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe a metragem do lado do terreno: ");
+            double lado = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe o % de acréscimo: ");
+            double acrescimo = double.Parse(Console.ReadLine());
+
+            double metragem = ((frente*lado) / 100) * acrescimo + (frente*lado);
+            
+            Console.Write("A Metragem a ser comprada é: " + metragem.ToString("F3"));
+
+            Console.ReadKey();
+        }
+    }
+}
